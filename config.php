d65ccf3ad84e05b24d40a5b8b761ca5d1d39f4f0
Alicia Venter <85114401+sphinxlet@users.noreply.github.com>
2025-02-19 10:01:32 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-    define('SERVER','localhost'); 
-    define('USERNAME','postgres');
-    define('PASSWORD','password');
-    define('DATABASE','postgres');
+    define('SERVER','your_database_server'); 
+    define('USERNAME','your_database_username');
+    define('PASSWORD','your_database_password');
+    define('DATABASE','your_database_name');
